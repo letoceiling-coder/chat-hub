597eb33ddfef9da7ce3966a2b05da3bd5f2564cc
letoceiling-coder <dsc-23@yandex.ru>
2026-02-02 02:04:04 +0300

    chore: add htaccess and configure production build

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,18 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
     hmr: {
       overlay: false,
     },
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: "esbuild",
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
